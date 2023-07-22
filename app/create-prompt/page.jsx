d00fc8c0040975eded050dcdefd01efa9b724708
Maxim <toper.one111@gmail.com>
2023-07-22 17:22:30 +0300

    Create a form submitting function to create a new prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import Form from '@/components/Form';
 
@@ -13,7 +13,32 @@ const CreatePrompt = () => {
     tag: '',
   });
 
-  const createPrompt = async (e) => {};
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user?.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
