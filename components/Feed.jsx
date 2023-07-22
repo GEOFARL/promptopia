bfa7c505cb670dab0bc66b180a11829527376220
Maxim <toper.one111@gmail.com>
2023-07-22 17:55:23 +0300

    Create a basic markup for feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,38 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      <h1></h1>
+    </div>
+  );
+};
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
