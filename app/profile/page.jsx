0931900e4f270107b6c1173e075b310b832c03bf
Maxim <toper.one111@gmail.com>
2023-07-23 09:51:12 +0300

    Add deleting posts functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -13,10 +13,30 @@ const MyProfile = () => {
   const router = useRouter();
 
   const handleEdit = (post) => {
-    router.push(`/update-prompt?id=${post._id}`);
+    router.push(`/update-prompt?id=${post._id.toString()}`);
   };
 
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      'Are you sure you want to delete this prompt?'
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter(
+          (p) => p._id.toString() !== post._id.toString()
+        );
+
+        setPosts(filteredPosts);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
